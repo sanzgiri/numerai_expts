bbd0492308e123cae2176d35208349a4ab04204a
Ashutosh Sanzgiri <asanzgiri@appnexus.com>
2017-03-04 23:12:58 +0000

    adding initial model files

---
+++ numerai_gb.py
@@ -0,0 +1,60 @@
+from sklearn.decomposition import FastICA, PCA, NMF  
+from sklearn.ensemble import GradientBoostingClassifier  
+from sklearn.svm import SVC  
+import numpy as np  
+import pandas as pd  
+from sklearn.linear_model import LogisticRegression  
+from sklearn.naive_bayes import GaussianNB  
+from sklearn.ensemble import RandomForestClassifier  
+from sklearn.ensemble import VotingClassifier  
+from sklearn.neural_network import BernoulliRBM  
+from sklearn.pipeline import Pipeline  
+from sklearn import preprocessing  
+from sklearn.calibration import CalibratedClassifierCV  
+from sklearn.cluster import MiniBatchKMeans
+
+df = pd.read_csv("/home/asanzgiri/my_numerai/numerai_training_data.csv")
+training = df.values[:, :50]  
+classes = df.values[:, -1]  
+training = preprocessing.scale(training)  
+kmeans = MiniBatchKMeans(n_clusters=500, init_size=6000).fit(training)  
+labels = kmeans.predict(training)
+
+clusters = {}  
+for i in range(0, np.shape(training)[0]):  
+    label = labels[i]  
+    if label not in clusters:  
+        clusters[label] = training[i, :]  
+    else:  
+        clusters[label] = np.vstack((clusters[label], training[i, :]))
+
+params = {'n_estimators': 1000, 'max_depth': 3, 'subsample': 0.5,  
+          'learning_rate': 0.01, 'min_samples_leaf': 1, 'random_state': 3}  
+gbm = GradientBoostingClassifier(**params)  
+ica = FastICA(10)
+
+icas = {}  
+for label in clusters:  
+    icas[label] = ica.fit(clusters[label])
+
+factors = np.zeros((np.shape(training)[0], 10))
+
+for i in range(0, np.shape(training)[0]):  
+    factors[i, :] = icas[labels[i]].transform(training[i, :].reshape(1, -1))
+
+gbm = gbm.fit(factors, classes)    
+
+tf = pd.read_csv("/home/asanzgiri/my_numerai/numerai_tournament_data.csv")  
+forecast = tf.values[:, 1:]  
+forecast = preprocessing.scale(forecast)  
+labels = kmeans.predict(forecast)
+
+factors = np.zeros((np.shape(labels)[0], 10))  
+for i, label in enumerate(labels):  
+    factors[i, :] = icas[label].transform(forecast[i, :].reshape(1, -1))
+
+proba = gbm.predict_proba(factors)
+
+of = pd.Series(proba[:, 1], index=tf.values[:, 0].astype(int))
+
+of.to_csv("/home/asanzgiri/my_numerai/predictions_s1.csv", header=['probability'], index_label='t_id')  
---
+++ numerai_xgb_grid.py
@@ -0,0 +1,234 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#Import libraries:
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+from sklearn import cross_validation, metrics   #Additional scklearn functions
+from sklearn.grid_search import GridSearchCV   #Perforing grid search
+
+#import matplotlib.pylab as plt
+#get_ipython().magic(u'matplotlib inline')
+#from matplotlib.pylab import rcParams
+#rcParams['figure.figsize'] = 12, 4
+
+train = pd.read_csv('/home/asanzgiri/my_numerai/numerai_training_data.csv')
+target = 'target'
+
+
+# In[2]:
+
+def modelfit(alg, dtrain, predictors,useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
+    
+    if useTrainCV:
+        xgb_param = alg.get_xgb_params()
+        xgtrain = xgb.DMatrix(dtrain[predictors].values, label=dtrain[target].values)
+        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
+            metrics='auc', early_stopping_rounds=early_stopping_rounds)
+        alg.set_params(n_estimators=cvresult.shape[0])
+    
+    #Fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain['target'],eval_metric='auc')
+        
+    #Predict training set:
+    dtrain_predictions = alg.predict(dtrain[predictors])
+    dtrain_predprob = alg.predict_proba(dtrain[predictors])[:,1]
+        
+    #Print model report:
+    print "\nModel Report"
+    print "Accuracy : %.4g" % metrics.accuracy_score(dtrain['target'].values, dtrain_predictions)
+    print "AUC Score (Train): %f" % metrics.roc_auc_score(dtrain['target'], dtrain_predprob)
+                    
+    feat_imp = pd.Series(alg.booster().get_fscore()).sort_values(ascending=False)
+ #   feat_imp.plot(kind='bar', title='Feature Importances')
+ #   plt.ylabel('Feature Importance Score')
+
+
+# In[ ]:
+
+#Choose all predictors except target & IDcols
+predictors = [x for x in train.columns if x not in [target]]
+xgb1 = XGBClassifier(
+ learning_rate =0.1,
+ n_estimators=1000,
+ max_depth=5,
+ min_child_weight=1,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'binary:logistic',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb1, train, predictors)
+
+
+# In[ ]:
+
+param_test1 = {
+ 'max_depth':range(3,10,2),
+ 'min_child_weight':range(1,6,2)
+}
+gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=140, max_depth=5,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
+ param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch1.fit(train[predictors],train[target])
+gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_
+
+
+# In[ ]:
+
+param_test2 = {
+ 'max_depth':[4,5,6],
+ 'min_child_weight':[4,5,6]
+}
+gsearch2 = GridSearchCV(estimator = XGBClassifier( learning_rate=0.1, n_estimators=140, max_depth=5,
+ min_child_weight=2, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test2, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch2.fit(train[predictors],train[target])
+gsearch2.grid_scores_, gsearch2.best_params_, gsearch2.best_score_
+
+
+# In[ ]:
+
+param_test2b = {
+ 'min_child_weight':[6,8,10,12]
+}
+gsearch2b = GridSearchCV(estimator = XGBClassifier( learning_rate=0.1, n_estimators=140, max_depth=4,
+ min_child_weight=2, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test2b, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch2b.fit(train[predictors],train[target])
+modelfit(gsearch3.best_estimator_, train, predictors)
+gsearch2b.grid_scores_, gsearch2b.best_params_, gsearch2b.best_score_
+
+
+# In[ ]:
+
+param_test3 = {
+ 'gamma':[i/10.0 for i in range(0,5)]
+}
+gsearch3 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=140, max_depth=4,
+ min_child_weight=6, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test3, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch3.fit(train[predictors],train[target])
+gsearch3.grid_scores_, gsearch3.best_params_, gsearch3.best_score_
+
+
+# In[ ]:
+
+xgb2 = XGBClassifier(
+ learning_rate =0.1,
+ n_estimators=1000,
+ max_depth=4,
+ min_child_weight=6,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'binary:logistic',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb2, train, predictors)
+
+
+# In[ ]:
+
+param_test4 = {
+ 'subsample':[i/10.0 for i in range(6,10)],
+ 'colsample_bytree':[i/10.0 for i in range(6,10)]
+}
+gsearch4 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test4, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch4.fit(train[predictors],train[target])
+gsearch4.grid_scores_, gsearch4.best_params_, gsearch4.best_score_
+
+
+# In[ ]:
+
+param_test5 = {
+ 'subsample':[i/100.0 for i in range(75,90,5)],
+ 'colsample_bytree':[i/100.0 for i in range(75,90,5)]
+}
+gsearch5 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test5, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch5.fit(train[predictors],train[target])
+
+
+# In[ ]:
+
+param_test6 = {
+ 'reg_alpha':[1e-5, 1e-2, 0.1, 1, 100]
+}
+gsearch6 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0.1, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test6, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch6.fit(train[predictors],train[target])
+gsearch6.grid_scores_, gsearch6.best_params_, gsearch6.best_score_
+
+
+# In[ ]:
+
+param_test7 = {
+ 'reg_alpha':[0, 0.001, 0.005, 0.01, 0.05]
+}
+gsearch7 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0.1, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test7, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch7.fit(train[predictors],train[target])
+gsearch7.grid_scores_, gsearch7.best_params_, gsearch7.best_score_
+
+
+# In[ ]:
+
+xgb3 = XGBClassifier(
+ learning_rate =0.1,
+ n_estimators=1000,
+ max_depth=4,
+ min_child_weight=6,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ reg_alpha=0.005,
+ objective= 'binary:logistic',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb3, train, predictors)
+
+
+# In[ ]:
+
+xgb4 = XGBClassifier(
+ learning_rate =0.01,
+ n_estimators=5000,
+ max_depth=4,
+ min_child_weight=6,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ reg_alpha=0.005,
+ objective= 'binary:logistic',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb4, train, predictors)
+
+
+# In[ ]:
+
+
+
